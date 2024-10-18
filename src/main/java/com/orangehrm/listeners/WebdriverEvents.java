0a1aa44d2c0dfedf9523725eea850dc98ea7c8fa
AneenaPVQA <pvaneena@gmail.com>
2024-10-18 19:54:38 -0400

    orangehrm updated

--- src/main/java/com/orangehrm/listeners/WebdriverEvents.java
+++ src/main/java/com/orangehrm/listeners/WebdriverEvents.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.openqa.selenium.support.events.WebDriverListener;
 
 import com.orangehrm.TestBase.TestBase;
 
